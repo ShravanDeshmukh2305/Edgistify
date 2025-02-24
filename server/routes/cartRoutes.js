31ca1e520b2e24c46a8b3db42d84ee4f6652f8d9
ShravanDeshmukh2305 <shravandeshmukh2305@gmail.com>
2025-02-24 16:34:26 +0530

    Added .gitignore

--- server/routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addToCart, getCart } = require('../controllers/cartController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/add', authMiddleware, addToCart);
-router.get('/get', authMiddleware, getCart)
-
-module.exports = router;
--- server/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { placeOrder } = require('../controllers/orderController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/place', authMiddleware, placeOrder);
-
-module.exports = router;
